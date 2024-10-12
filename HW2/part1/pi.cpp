03b4a4310ca4e786fe029f7e80bfa554dba42e0a
Ray Huang <bjhuang@cs.nycu.edu.tw>
2024-10-12 20:21:28 +0800

    feat: speedup HW2 part1 for fitting time limit

--- HW2/part1/pi.cpp
+++ HW2/part1/pi.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <random>
-#include <ctime>
 #include <pthread.h>
-#include <cstdlib>
 #include <unistd.h>
+#include <vector>
 
 using namespace std;
 
@@ -12,30 +11,36 @@ struct thread_data{
     int tid;
 };
 
-long long *num_in_circle_part;
+struct alignas(64) padded_long {
+    long long value;
+};
+
+vector<padded_long> *num_in_circle_part;
 
 void *tossing(void *data){
     struct thread_data *td = (struct thread_data *)data;
     int tid = td->tid;
     long long num_of_toss = td->tosses;
     long long number_in_circle = 0;
-    random_device rd;
-    mt19937_64 generater(rd());
+    minstd_rand generator(tid);
     uniform_real_distribution<double> unif(-1.0, 1.0);
     for(long long toss = 0; toss < num_of_toss; toss++){
-        double x = unif(generater);
-        double y = unif(generater);
+        double x = unif(generator);
+        double y = unif(generator);
         double distance_squared = x*x + y*y;
         if(distance_squared <= 1.0){
             number_in_circle++;
         }
     }
     
-    num_in_circle_part[tid] = number_in_circle;
+    (*num_in_circle_part)[tid].value = number_in_circle;
     return nullptr;
 }
 
 int main(int argc, char *argv[]){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
     if (argc != 3){
         cout << "Usage: " << argv[0] << "<number of threads> <number of tosses>\n";
         return 1;
@@ -44,13 +49,10 @@ int main(int argc, char *argv[]){
     int number_of_threads = atoi(argv[1]);
     long long number_of_tosses = atoll(argv[2]);
     double pi_estimate;
-    random_device rd;
-    mt19937_64 generater(rd());
-    uniform_real_distribution<double> unif(-1.0, 1.0);
 
-    pthread_t threads[number_of_threads];
-    num_in_circle_part = (long long *)malloc(sizeof(long long) * number_of_threads);
-    struct thread_data *td = (struct thread_data *)malloc(sizeof(struct thread_data) * number_of_threads);
+    vector<pthread_t> threads(number_of_threads);
+    num_in_circle_part = new vector<padded_long>(number_of_threads);
+    vector<thread_data> td(number_of_threads);
     long long tosses_per_thread = number_of_tosses / number_of_threads;
     for(int i = 0; i < number_of_threads; i++){
         td[i].tosses = tosses_per_thread;
@@ -59,9 +61,11 @@ int main(int argc, char *argv[]){
     }
     for(int i = 0; i < number_of_threads; i++){
         pthread_join(threads[i], NULL);
-        number_in_circle += num_in_circle_part[i];
+        number_in_circle += (*num_in_circle_part)[i].value;
     }
-    pi_estimate = 4 * number_in_circle / ((double)number_of_tosses);
+    pi_estimate = 4 * number_in_circle / static_cast<double>(number_of_tosses);
     cout << pi_estimate << '\n';
+
+    delete num_in_circle_part;
     return 0;
 }
