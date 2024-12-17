c31aec654b53dc748048dfe1434132487011b59b
Ray Huang <bjhuang@cs.nycu.edu.tw>
2024-12-17 16:41:45 +0800

    feat: HW6 done (no bonus)

--- HW6/hostFE.c
+++ HW6/hostFE.c
@@ -9,5 +9,32 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
 {
     cl_int status;
     int filterSize = filterWidth * filterWidth;
+    int imageSize = imageHeight * imageWidth;
 
+    // create buffers for image, filter and output
+    cl_mem imageBuffer = clCreateBuffer(*context, CL_MEM_USE_HOST_PTR, imageSize * sizeof(float), inputImage, &status);
+    cl_mem filterBuffer = clCreateBuffer(*context, CL_MEM_USE_HOST_PTR, filterSize * sizeof(float), filter, &status);
+    cl_mem outputBuffer = clCreateBuffer(*context, CL_MEM_WRITE_ONLY, imageSize * sizeof(float), NULL, &status);
+
+    // create kernel
+    cl_kernel kernel = clCreateKernel(*program, "convolution", &status);
+
+    // set args
+    status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &imageBuffer);
+    status = clSetKernelArg(kernel, 1, sizeof(cl_mem), &outputBuffer);
+    status = clSetKernelArg(kernel, 2, sizeof(cl_mem), &filterBuffer);
+    status = clSetKernelArg(kernel, 3, sizeof(int), &imageHeight);
+    status = clSetKernelArg(kernel, 4, sizeof(int), &imageWidth);
+    status = clSetKernelArg(kernel, 5, sizeof(int), &filterWidth);
+
+    // create command queue
+    cl_command_queue queue = clCreateCommandQueue(*context, *device, 0, &status);
+
+    // enqueue kernel
+    const size_t globalSize[2] = {imageWidth, imageHeight};
+    const size_t localSize[2] = {20, 20};
+    status = clEnqueueNDRangeKernel(queue, kernel, 2, NULL, &globalSize, &localSize, 0, NULL, NULL);
+
+    // read output buffer
+    status = clEnqueueReadBuffer(queue, outputBuffer, CL_TRUE, 0,imageSize * sizeof(float), outputImage, 0, NULL, NULL);
 }
--- HW6/kernel.cl
+++ HW6/kernel.cl
@@ -1,4 +1,33 @@
-__kernel void convolution() 
+__kernel void convolution(
+    const __global float* inputImage,
+    __global float* outputImage,
+    const __global float* filter,
+    const int imageHeight,
+    const int imageWidth,
+    const int filterWidth)
 {
-   
+    int x = get_global_id(0);
+    int y = get_global_id(1);
+    int halfFilterSize = filterWidth / 2;
+    int k, l;
+    float sum = 0.0f;
+
+    // iterate over the filter
+    for (k = -halfFilterSize;k <= halfFilterSize;k++) {
+        for (l = -halfFilterSize; l <= halfFilterSize; l++)
+        {
+            if(filter[(k + halfFilterSize) * filterWidth + l + halfFilterSize] != 0)
+            {
+                if (y + k >= 0 && y + k < imageHeight &&
+                    x + l >= 0 && x + l < imageWidth)
+                {
+                    sum += inputImage[(y + k) * imageWidth + x + l] *
+                            filter[(k + halfFilterSize) * filterWidth +
+                                    l + halfFilterSize];
+                }
+            }
+        }
+    }
+
+    outputImage[y * imageWidth + x] = sum;
 }
